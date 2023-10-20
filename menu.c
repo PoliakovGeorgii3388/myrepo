74ebb3d9d869cf8919bcd4d31e24b506670474f9
PoliakovGeorgii3388 <144445285+PoliakovGeorgii3388@users.noreply.github.com>
2023-10-20 11:47:46 +0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "data_input.h"
 #include "data_output.h"
-#define max_arr_len 100
+#define MAX_ARR_LEN 100
 int main()
 {
     int len_arr = 0;
-    int array_nums[max_arr_len];
+    int array_nums[MAX_ARR_LEN];
     data_input(array_nums, &len_arr);
     data_output(array_nums, len_arr);
     return 0;
